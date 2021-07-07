b6bf6842d33dace27a207a1027e76e8e073e6d84
Taylor Jadin <jadin.taylor@gmail.com>
2021-07-07 08:08:58 -0500

    mister rom syncing

--- romSync.sh
+++ romSync.sh
@@ -4,8 +4,9 @@
 piboy='piboy'
 gpi='gpi'
 mister='mister.jadin.me'
-unraid_roms='/mnt/user/media/ROMs/'
-retropie_roms='/home/pi/RetroPie/roms/'
+unraid_roms='/mnt/user/media/ROMs'
+retropie_roms='/home/pi/RetroPie/roms'
+mister_roms='/media/fat/games'
 sync="rsync -rPt --exclude-from=/mnt/user/appdata/romSync/exclude.txt"
 mirror="rsync -rPt --del --exclude-from=/mnt/user/appdata/romSync/exclude.txt"
 backup="rsync -ahP --del"
@@ -17,21 +18,33 @@ pull() {
   then
     echo ""
     echo "--> Syncing roms from $piboy"
-    $sync pi@$piboy:$retropie_roms $unraid_roms
+    $sync pi@$piboy:$retropie_roms/ $unraid_roms/
   fi
 
   if ping -c 1 $gpi &> /dev/null
   then
     echo ""
     echo "--> Syncing roms from $gpi"
-    $sync pi@$gpi:$retropie_roms $unraid_roms
+    $sync pi@$gpi:$retropie_roms/ $unraid_roms/
   fi
 
   if ping -c 1 $mister &> /dev/null
   then
     echo ""
     echo "--> Syncing roms from $mister"
-    $sync root@$mister:/media/fat/games/ATARI2600/ /mnt/user/media/ROMs/atari2600/
+    $sync root@$mister:$mister_roms/ATARI2600/ $unraid_roms/atari2600/
+    $sync root@$mister:$mister_roms/ATARI5200/ $unraid_roms/atari5200/
+    $sync root@$mister:$mister_roms/ATARI7800/ $unraid_roms/atari7800/
+    $sync root@$mister:$mister_roms/AtariLynx/ $unraid_roms/atarilynx/
+    $sync root@$mister:$mister_roms/GAMEBOY/GB/ $unraid_roms/gb/
+    $sync root@$mister:$mister_roms/GAMEBOY/GBC/ $unraid_roms/gbc/
+    $sync root@$mister:$mister_roms/GBA/ $unraid_roms/gba/
+    $sync root@$mister:$mister_roms/Genesis/ $unraid_roms/megadrive/
+    $sync root@$mister:$mister_roms/NeoGeo/ $unraid_roms/neogeo/
+    $sync root@$mister:$mister_roms/NES/ $unraid_roms/nes/
+    $sync root@$mister:$mister_roms/SMS/ $unraid_roms/mastersystem/
+    $sync root@$mister:$mister_roms/SNES/ $unraid_roms/snes/
+    $sync root@$mister:$mister_roms/TGFX16/ $unraid_roms/pcengine/
   fi
 }
 
@@ -40,21 +53,33 @@ push() {
   then
     echo ""
     echo "--> Mirroring from unraid to $piboy"
-    $mirror $unraid_roms pi@$piboy:$retropie_roms
+    $mirror $unraid_roms/ pi@$piboy:$retropie_roms/
   fi
 
   if ping -c 1 $gpi &> /dev/null
   then
     echo ""
     echo "--> Mirroring from unraid to $gpi"
-    $mirror $unraid_roms pi@$gpi:$retropie_roms
+    $mirror $unraid_roms/ pi@$gpi:$retropie_roms/
   fi
 
   if ping -c 1 $mister &> /dev/null
   then
     echo ""
     echo "--> Mirroring from unraid to $mister"
-    $mirror /mnt/user/media/ROMs/atari2600/ root@$mister:/media/fat/games/ATARI2600/
+    $mirror $unraid_roms/atari2600/ root@$mister:$mister_roms/ATARI2600/
+    $mirror $unraid_roms/atari5200/ root@$mister:$mister_roms/ATARI5200/
+    $mirror $unraid_roms/atari7800/ root@$mister:$mister_roms/ATARI7800/
+    $mirror $unraid_roms/atarilynx/ root@$mister:$mister_roms/AtariLynx/
+    $mirror $unraid_roms/gb/ root@$mister:$mister_roms/GAMEBOY/GB/
+    $mirror $unraid_roms/gbc/ root@$mister:$mister_roms/GAMEBOY/GBC/
+    $mirror $unraid_roms/gba/ root@$mister:$mister_roms/GBA/
+    $mirror $unraid_roms/megadrive/ root@$mister:$mister_roms/Genesis/
+    $mirror $unraid_roms/neogeo/ root@$mister:$mister_roms/NeoGeo/
+    $mirror $unraid_roms/nes/ root@$mister:$mister_roms/NES/
+    $mirror $unraid_roms/mastersystem/ root@$mister:$mister_roms/SMS/
+    $mirror $unraid_roms/snes/ root@$mister:$mister_roms/SNES/
+    $mirror $unraid_roms/pcengine/ root@$mister:$mister_roms/TGFX16/
   fi
 }
 

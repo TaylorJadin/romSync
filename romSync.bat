47fbca9b92182610b3bb99767409ab88082e4786
Taylor Jadin <jadin.taylor@gmail.com>
2023-01-14 20:31:47 -0600

    romSync.bat

---
+++ romSync.bat
@@ -0,0 +1,3 @@
+robocopy /mir C:\Users\jadin\scoop\apps\rpcs3\current\dev_hdd0\home\ Y:\taylor-compy\rpcs3\dev_hdd0\home\
+robocopy /mir C:\Users\jadin\Documents\Cemu\mlc01\ Y:\taylor-compy\cemu\mlc01\
+ssh root@unraid.jadin.me /mnt/user/appdata/romSync/romSync.sh

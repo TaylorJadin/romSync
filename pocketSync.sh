c6bcbe43d563aa602fa198c3e4f98eebf91a50e8
Taylor Jadin <jadin.taylor@gmail.com>
2023-01-25 07:49:56 -0600

    stat script for analogue pocket

---
+++ pocketSync.sh
@@ -0,0 +1,24 @@
+rom_copy="rsync -ri --delete --ignore-existing --exclude-from=./exclude.txt"
+unraid_games='/mnt/user/games'
+pocket_sd='/Volumes/PocketSD'
+
+# $rom_copy $unraid_games/roms/ATARI2600/ $pocket_sd/Assets/atari2600/
+# $rom_copy $unraid_games/roms/ATARI5200/ $pocket_sd/Assets/atari5200/
+# $rom_copy $unraid_games/roms/ATARI7800/ $pocket_sd/Assets/atari7800/
+# $rom_copy $unraid_games/roms/AtariJaguar/ $pocket_sd/Assets/atarijaguar/
+# $rom_copy $unraid_games/roms/AtariLynx/ $pocket_sd/Assets/atarilynx/
+# $rom_copy $unraid_games/roms/Coleco/Colecovision/ $pocket_sd/Assets/colecovision/
+# $rom_copy $unraid_games/roms/Coleco/SG1000/ $pocket_sd/Assets/sg-1000/
+# $rom_copy $unraid_games/roms/GAMEBOY/1\ Game\ Boy/ $pocket_sd/Assets/gb/
+# $rom_copy $unraid_games/roms/GAMEBOY/1\ Game\ Boy\ Color/ $pocket_sd/Assets/gbc/
+# $rom_copy $unraid_games/roms/GBA/ $pocket_sd/Assets/gba/
+# $rom_copy $unraid_games/roms/Genesis/ $pocket_sd/Assets/genesis/
+# $rom_copy $unraid_games/roms/N64/ $pocket_sd/Assets/n64/
+# $rom_copy $unraid_games/roms/NEOGEO/ $pocket_sd/Assets/neogeo/
+# $rom_copy $unraid_games/roms/NeoGeoPocketColor/1\ NGPC\ US\ -\ A-Z/ $pocket_sd/Assets/ngpc/
+# $rom_copy $unraid_games/roms/NeoGeoPocketColor/3\ NGP\ -\ A-Z/ $pocket_sd/Assets/ngp/
+# $rom_copy $unraid_games/roms/NES/ $pocket_sd/Assets/nes/
+# $rom_copy $unraid_games/roms/S32X/ $pocket_sd/Assets/sega32x/
+# $rom_copy $unraid_games/roms/SMS/ $pocket_sd/Assets/mastersystem/
+# $rom_copy $unraid_games/roms/SNES/ $pocket_sd/Assets/snes/
+# $rom_copy $unraid_games/roms/TGFX16/ $pocket_sd/Assets/tg16/

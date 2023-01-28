11e5cd80cc7a3246bf23cc818f0ce138b2c3887d
Taylor Jadin <jadin.taylor@gmail.com>
2023-01-28 08:23:35 -0600

    pocketSync.sh

--- pocketSync.sh
+++ pocketSync.sh
@@ -2,23 +2,17 @@ rom_copy="rsync -ri --delete --ignore-existing --exclude-from=./exclude.txt"
 unraid_games='/mnt/user/games'
 pocket_sd='/Volumes/PocketSD'
 
-# $rom_copy $unraid_games/roms/ATARI2600/ $pocket_sd/Assets/atari2600/
-# $rom_copy $unraid_games/roms/ATARI5200/ $pocket_sd/Assets/atari5200/
-# $rom_copy $unraid_games/roms/ATARI7800/ $pocket_sd/Assets/atari7800/
-# $rom_copy $unraid_games/roms/AtariJaguar/ $pocket_sd/Assets/atarijaguar/
-# $rom_copy $unraid_games/roms/AtariLynx/ $pocket_sd/Assets/atarilynx/
-# $rom_copy $unraid_games/roms/Coleco/Colecovision/ $pocket_sd/Assets/colecovision/
-# $rom_copy $unraid_games/roms/Coleco/SG1000/ $pocket_sd/Assets/sg-1000/
-# $rom_copy $unraid_games/roms/GAMEBOY/1\ Game\ Boy/ $pocket_sd/Assets/gb/
-# $rom_copy $unraid_games/roms/GAMEBOY/1\ Game\ Boy\ Color/ $pocket_sd/Assets/gbc/
-# $rom_copy $unraid_games/roms/GBA/ $pocket_sd/Assets/gba/
-# $rom_copy $unraid_games/roms/Genesis/ $pocket_sd/Assets/genesis/
-# $rom_copy $unraid_games/roms/N64/ $pocket_sd/Assets/n64/
-# $rom_copy $unraid_games/roms/NEOGEO/ $pocket_sd/Assets/neogeo/
-# $rom_copy $unraid_games/roms/NeoGeoPocketColor/1\ NGPC\ US\ -\ A-Z/ $pocket_sd/Assets/ngpc/
-# $rom_copy $unraid_games/roms/NeoGeoPocketColor/3\ NGP\ -\ A-Z/ $pocket_sd/Assets/ngp/
-# $rom_copy $unraid_games/roms/NES/ $pocket_sd/Assets/nes/
-# $rom_copy $unraid_games/roms/S32X/ $pocket_sd/Assets/sega32x/
-# $rom_copy $unraid_games/roms/SMS/ $pocket_sd/Assets/mastersystem/
-# $rom_copy $unraid_games/roms/SNES/ $pocket_sd/Assets/snes/
-# $rom_copy $unraid_games/roms/TGFX16/ $pocket_sd/Assets/tg16/
+$rom_copy $unraid_games/roms/ATARI2600/ $pocket_sd/Assets/atari2600/common/
+$rom_copy $unraid_games/roms/ATARI7800/ $pocket_sd/Assets/7800/common/
+$rom_copy $unraid_games/roms/Coleco/Colecovision/ $pocket_sd/Assets/colecovision/common/
+$rom_copy $unraid_games/roms/Coleco/SG1000/ $pocket_sd/Assets/sg-1000/common/
+$rom_copy $unraid_games/roms/GAMEBOY/1\ Game\ Boy/ $pocket_sd/Assets/gb/common/
+$rom_copy $unraid_games/roms/GAMEBOY/1\ Game\ Boy\ Color/ $pocket_sd/Assets/gbc/common/
+$rom_copy $unraid_games/roms/GBA/ $pocket_sd/Assets/gba/common/
+$rom_copy $unraid_games/roms/Genesis/ $pocket_sd/Assets/genesis/common/
+$rom_copy $unraid_games/roms/NEOGEO/ $pocket_sd/Assets/ng/common/
+$rom_copy $unraid_games/roms/NES/ $pocket_sd/Assets/nes/common/
+$rom_copy $unraid_games/roms/SMS/ $pocket_sd/Assets/sms/common/
+$rom_copy $unraid_games/roms/SNES/ $pocket_sd/Assets/snes/common/
+$rom_copy $unraid_games/roms/TGFX16/ $pocket_sd/Assets/pce/common/
+$rom_copy $unraid_games/roms/WonderSwan/1\ WonderSwan\ A-Z/ $pocket_sd/Assets/wonderswan/common/

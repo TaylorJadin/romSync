59df30ae2ca16dc43fe8fff40ae242d71007a4c4
Taylor Jadin <jadin.taylor@gmail.com>
2023-03-24 00:19:15 -0500

    archive symlinks

--- romSync.sh
+++ romSync.sh
@@ -8,7 +8,7 @@ unraid_games='/mnt/user/games'
 retropie_home='/home/pi/RetroPie'
 mister_sd='/media/fat'
 deck_emufolder='/home/deck/Emulation'
-save_sync="rsync -ri --times --update"
+save_sync="rsync -ai --times --update"
 rom_copy="rsync -ri --delete --ignore-existing --exclude-from=/mnt/user/appdata/romSync/exclude.txt"
 
 ### Functions ###

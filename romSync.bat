cbb7340922dc368f16c41f905f27e22779d6ba37
Taylor Jadin <jadin.taylor@gmail.com>
2023-03-23 23:52:00 -0500

    Update romSync.bat

--- romSync.bat
+++ romSync.bat
@@ -1,3 +1,2 @@
-robocopy /mir C:\Users\jadin\scoop\apps\rpcs3\current\dev_hdd0\home\ Y:\taylor-compy\rpcs3\dev_hdd0\home\
-robocopy /mir C:\Users\jadin\Documents\Cemu\mlc01\ Y:\taylor-compy\cemu\mlc01\
+robocopy /mir C:\Users\jadin\scoop\persist\rpcs3\dev_hdd0\home\ Y:\taylor-compy\rpcs3\dev_hdd0\home\
 ssh root@unraid.jadin.me /mnt/user/appdata/romSync/romSync.shlu

2cee6185e10013d027b44aeddd91b3bcdf3f3823
Taylor Jadin <jadin.taylor@gmail.com>
2023-03-24 00:17:44 -0500

    fix fix

--- romSync.bat
+++ romSync.bat
@@ -1,2 +1,2 @@
 robocopy /mir C:\Users\jadin\scoop\persist\rpcs3\dev_hdd0\home\ Y:\taylor-compy\rpcs3\dev_hdd0\home\
-ssh root@unraid.jadin.me /mnt/user/appdata/romSync/romSync.shlu
+ssh root@unraid.jadin.me /mnt/user/appdata/romSync/romSync.sh

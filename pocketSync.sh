345e6ad52c18e67cdb6613c4b7e1dabfb50a7934
Taylor Jadin <jadin.taylor@gmail.com>
2023-12-20 00:22:03 -0600

    romsync paths

--- pocketSync.sh
+++ pocketSync.sh
@@ -2,17 +2,17 @@ rom_copy="rsync -ri --ignore-existing --exclude-from=./exclude.txt"
 unraid_games=/Volumes/games
 pocket_sd=/Volumes/PocketSD
 
-$rom_copy $unraid_games/roms/ATARI2600/ $pocket_sd/Assets/2600/common/
-$rom_copy $unraid_games/roms/ATARI7800/ $pocket_sd/Assets/7800/common/
-$rom_copy $unraid_games/roms/Coleco/Colecovision/ $pocket_sd/Assets/coleco/common/
-$rom_copy $unraid_games/roms/Coleco/SG1000/ $pocket_sd/Assets/sg1000/common/
-$rom_copy $unraid_games/roms/GAMEBOY/1\ Game\ Boy/ $pocket_sd/Assets/gb/common/
-$rom_copy $unraid_games/roms/GAMEBOY/1\ Game\ Boy\ Color/ $pocket_sd/Assets/gbc/common/
-$rom_copy $unraid_games/roms/GBA/ $pocket_sd/Assets/gba/common/
-$rom_copy $unraid_games/roms/Genesis/ $pocket_sd/Assets/genesis/common/
-$rom_copy $unraid_games/roms/NEOGEO/ $pocket_sd/Assets/ng/common/
-$rom_copy $unraid_games/roms/NES/ $pocket_sd/Assets/nes/common/
-$rom_copy $unraid_games/roms/SMS/ $pocket_sd/Assets/sms/common/
-$rom_copy $unraid_games/roms/SNES/ $pocket_sd/Assets/snes/common/
-$rom_copy $unraid_games/roms/TGFX16/ $pocket_sd/Assets/pce/common/
-$rom_copy $unraid_games/roms/WonderSwan/1\ WonderSwan\ A-Z/ $pocket_sd/Assets/wonderswan/common/
+# $rom_copy $unraid_games/roms/ATARI2600/ $pocket_sd/Assets/2600/common/
+# $rom_copy $unraid_games/roms/ATARI7800/ $pocket_sd/Assets/7800/common/
+# $rom_copy $unraid_games/roms/Coleco/Colecovision/ $pocket_sd/Assets/coleco/common/
+# $rom_copy $unraid_games/roms/Coleco/SG1000/ $pocket_sd/Assets/sg1000/common/
+# $rom_copy $unraid_games/roms/GAMEBOY/1\ Game\ Boy/ $pocket_sd/Assets/gb/common/
+# $rom_copy $unraid_games/roms/GAMEBOY/1\ Game\ Boy\ Color/ $pocket_sd/Assets/gbc/common/
+# $rom_copy $unraid_games/roms/GBA/ $pocket_sd/Assets/gba/common/
+# $rom_copy $unraid_games/roms/Genesis/ $pocket_sd/Assets/genesis/common/
+# $rom_copy $unraid_games/roms/NEOGEO/ $pocket_sd/Assets/ng/common/
+# $rom_copy $unraid_games/roms/NES/ $pocket_sd/Assets/nes/common/
+# $rom_copy $unraid_games/roms/SMS/ $pocket_sd/Assets/sms/common/
+# $rom_copy $unraid_games/roms/SNES/ $pocket_sd/Assets/snes/common/
+# $rom_copy $unraid_games/roms/TGFX16/ $pocket_sd/Assets/pce/common/
+# $rom_copy $unraid_games/roms/WonderSwan/1\ WonderSwan\ A-Z/ $pocket_sd/Assets/wonderswan/common/

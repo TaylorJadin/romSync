9da0283dfa2502541e25a23236842f39a45a8c22
Taylor Jadin <jadin.taylor@gmail.com>
2023-12-21 17:14:02 -0600

    romsync bat path

--- romSync.bat
+++ romSync.bat
@@ -1,2 +1,2 @@
-robocopy /mir C:\Users\jadin\scoop\persist\rpcs3\dev_hdd0\home\ Y:\taylor-compy\rpcs3\dev_hdd0\home\
+robocopy /mir C:\Users\jadin\scoop\persist\rpcs3\dev_hdd0\home\ \\unraid.jadin.me\games\taylor-compy\rpcs3\dev_hdd0\home\
 ssh root@unraid.jadin.me /mnt/user/appdata/romSync/romSync.sh

4ee4c1a7757314ccbedcf733a81712a41482b4bb
Taylor Jadin <jadin.taylor@gmail.com>
2023-12-29 21:49:41 -0600

    pocketsync updates

--- pocketSync.sh
+++ pocketSync.sh
@@ -1,16 +1,15 @@
 rom_copy="rsync -ri --ignore-existing --exclude-from=./exclude.txt"
-unraid_games=/Volumes/games
-pocket_sd=/Volumes/PocketSD
+unraid_games=root@unraid.jadin.me:/mnt/user/games/roms
 
-$rom_copy $unraid_games/roms/atari2600/ $pocket_sd/Assets/2600/common/
-$rom_copy $unraid_games/roms/atari7800/ $pocket_sd/Assets/7800/common/
-$rom_copy $unraid_games/roms/gb/ $pocket_sd/Assets/gb/common/
-$rom_copy $unraid_games/roms/gbc/ $pocket_sd/Assets/gbc/common/
-$rom_copy $unraid_games/roms/gba/ $pocket_sd/Assets/gba/common/
-$rom_copy $unraid_games/roms/genesis/ $pocket_sd/Assets/genesis/common/
-$rom_copy $unraid_games/roms/neogeo_mister/ $pocket_sd/Assets/ng/common/
-$rom_copy $unraid_games/roms/nes/ $pocket_sd/Assets/nes/common/
-$rom_copy $unraid_games/roms/mastersystem/ $pocket_sd/Assets/sms/common/
-$rom_copy $unraid_games/roms/snes/ $pocket_sd/Assets/snes/common/
-$rom_copy $unraid_games/roms/tgfx16/ $pocket_sd/Assets/pce/common/
-$rom_copy $unraid_games/roms/wonderswan/ $pocket_sd/Assets/wonderswan/common/
+$rom_copy $unraid_games/atari2600/ ./Assets/2600/common/
+$rom_copy $unraid_games/atari7800/ ./Assets/7800/common/
+$rom_copy $unraid_games/gb/ ./Assets/gb/common/
+$rom_copy $unraid_games/gbc/ ./Assets/gbc/common/
+$rom_copy $unraid_games/gba/ ./Assets/gba/common/
+$rom_copy $unraid_games/genesis/ ./Assets/genesis/common/
+$rom_copy $unraid_games/neogeo_mister/ ./Assets/ng/common/
+$rom_copy $unraid_games/nes/ ./Assets/nes/common/
+$rom_copy $unraid_games/mastersystem/ ./Assets/sms/common/
+$rom_copy $unraid_games/snes/ ./Assets/snes/common/
+$rom_copy $unraid_games/tgfx16/ ./Assets/pce/common/
+$rom_copy $unraid_games/wonderswan/ ./Assets/wonderswan/common/
